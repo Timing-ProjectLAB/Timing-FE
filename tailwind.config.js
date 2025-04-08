86c1fc0b61106e26bd03d0ec2129a643d3848929
k1950035 <53138789+k1950035@users.noreply.github.com>
2025-04-08 13:11:06 +0900

    Initial Commit
    기본적으로 사용될 법한 의존성 및 Eslint 와 Prettier의 설정을 수행했습니다.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {
+        pre: ['Pretendard-Black'],
+      },
+    },
+  },
+  plugins: [],
+};
